f53a0ebcc4b2b93c04cfbea199baba3077544b63
Conrad Wood <cnw@conradwood.net>
2022-04-06 09:31:12 +0100

    added update repo status and allow repobuilder to check out ANY repo
    Change-Id: Iea310906c1c0ff8924537f3d0d757b7d744d84a8

---
+++ src/golang.conradwood.net/gitserver/git2/update_repo_status.go
@@ -0,0 +1,50 @@
+package git2
+
+import (
+	"context"
+	gitpb "golang.conradwood.net/apis/gitserver"
+	"golang.conradwood.net/go-easyops/auth"
+	"golang.conradwood.net/go-easyops/errors"
+)
+
+func (g *GIT2) UpdateRepoStatus(ctx context.Context, req *gitpb.UpdateRepoStatusRequest) (*gitpb.SourceRepository, error) {
+	u := auth.GetUser(ctx)
+	if u == nil {
+		return nil, errors.Unauthenticated(ctx, "please authenticate")
+	}
+	if !isrepobuilder(ctx) {
+		return nil, errors.AccessDenied(ctx, "access only for repobuilder")
+	}
+	if req.RepoID == 0 {
+		return nil, errors.InvalidArgs(ctx, "missing repository id", "missing repository id")
+	}
+	bir := &gitpb.ByIDRequest{ID: req.RepoID}
+	sr, err := g.RepoByID(ctx, bir)
+	if err != nil {
+		return nil, err
+	}
+	nb, set := decode_repostate(req.ReadOnly)
+	if set {
+		sr.ReadOnly = nb
+	}
+	nb, set = decode_repostate(req.RunHooks)
+	if set {
+		sr.RunPostReceive = nb
+		sr.RunPreReceive = nb
+	}
+	err = g.repo_store.Update(ctx, sr)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+func decode_repostate(i gitpb.NewRepoState) (bool, bool) {
+	if i == gitpb.NewRepoState_SET_TRUE {
+		return true, true
+	}
+	if i == gitpb.NewRepoState_SET_FALSE {
+		return false, true
+	}
+	return false, false
+}
