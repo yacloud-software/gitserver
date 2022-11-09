de09e6acca0f135ce21af9dc378bc79ab3a571b5
Conrad Wood <cnw@conradwood.net>
2022-11-09 19:26:14 +0000

    dded update repo
    Change-Id: I30561fb11bd81b77e17264557a24e679ffaa4e4e

--- src/golang.conradwood.net/gitserver/git2/update_repo_status.go
+++ src/golang.conradwood.net/gitserver/git2/update_repo_status.go
@@ -3,6 +3,7 @@ package git2
 import (
 	"context"
 	gitpb "golang.conradwood.net/apis/gitserver"
+	"strings"
 	//	"golang.conradwood.net/go-easyops/auth"
 	"golang.conradwood.net/go-easyops/errors"
 )
@@ -49,4 +50,55 @@ func decode_repostate(i gitpb.NewRepoState) (bool, bool) {
 		return false, true
 	}
 	return false, false
+}
+
+func (g *GIT2) GitRepoUpdate(ctx context.Context, req *gitpb.RepoUpdateRequest) (*gitpb.SourceRepository, error) {
+	if req.Original == nil || req.Original.ID == 0 {
+		return nil, errors.InvalidArgs(ctx, "missing repository id", "missing repository id")
+	}
+	ri, err := g.RepoByID(ctx, &gitpb.ByIDRequest{ID: req.Original.ID})
+	if err != nil {
+		return nil, err
+	}
+
+	if req.AddURLHost != "" && req.AddURLPath != "" {
+		cr, err := g.CheckGitServer(ctx, &gitpb.CheckGitRequest{Host: req.AddURLHost})
+		if err != nil {
+			return nil, err
+		}
+		if !cr.Success {
+			return nil, errors.InvalidArgs(ctx, "no gitserver found at host \"%s\"", req.AddURLHost)
+		}
+		path := req.AddURLPath
+		if strings.HasSuffix(path, ".git") {
+			return nil, errors.InvalidArgs(ctx, "paths must end with .git", "paths must end with .git")
+		}
+		// TODO: append .git if necessary, check for dupes etc
+		ur := &gitpb.SourceRepositoryURL{
+			V2RepositoryID: ri.ID,
+			Host:           req.AddURLHost,
+			Path:           path,
+		}
+		_, err = g.url_store.Save(ctx, ur)
+		if err != nil {
+			return nil, err
+		}
+		ri.URLs = append(ri.URLs, ur)
+	}
+
+	save := false
+	if req.Description != "" {
+		save = true
+		ri.Description = req.Description
+	}
+
+	if save {
+		err = g.repo_store.Update(ctx, ri)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return ri, nil
+
 }
